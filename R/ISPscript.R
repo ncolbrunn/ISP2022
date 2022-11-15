9d7f2ce540fef82ff37bcbcd73ca5e5165a6b9ab
Nate Colbrunn <nathan.colbrunn@hope.edu>
2022-11-15 14:17:08 -0300

    added code from app and cleaned up

--- R/ISPscript.R
+++ R/ISPscript.R
@@ -3,27 +3,78 @@ library(shiny)
 library(igraph)
 library(multiweb)
 library(dplyr)
+library(leaflet)
+library(measurements)
 
-#convert dataFrame into iGraph
+#prepare to convert dataFrame into iGraph
 dat <- read.csv("283_3_283_2_FoodWebDataBase_2018_12_10.csv", header = TRUE, na.strings="NA")
 foodWebDF <- dat %>% 
-  select(res.taxonomy, con.taxonomy, foodweb.name)
+  dplyr::select(res.taxonomy, con.taxonomy, foodweb.name, longitude, latitude, ecosystem.type, study.site) %>% 
+  dplyr::filter(!foodweb.name == "Carpinteria") %>%
+  dplyr::filter(!foodweb.name == "Weddell Sea")
 
-#unique(dat$foodweb.name)
+#remove duplicated foodwebs
+foodWebDF["ecosystem.type"][foodWebDF["study.site"] == "Grand Caricaie"] <- "terrestrial aboveground"
+
+#Split duplicated network and create distinct foodweb names
+carp_mar <- dat %>% 
+  dplyr::filter(foodweb.name == "Carpinteria" & ecosystem.type == "marine") %>% 
+  dplyr::select(res.taxonomy, con.taxonomy, foodweb.name, longitude, latitude, ecosystem.type, study.site)
+carp_mar$foodweb.name <- "Carpinteria Marine"
+
+carp_ter <- dat %>%
+  dplyr::filter(foodweb.name == "Carpinteria" & ecosystem.type == "terrestrial aboveground") %>% 
+  dplyr::select(res.taxonomy, con.taxonomy, foodweb.name, longitude, latitude, ecosystem.type, study.site)
+carp_ter$foodweb.name <- "Carpinteria Terrestrial"
+
+#merge dataframes
+foodWebDF_up <- rbind(foodWebDF, carp_mar)
+foodWebDF <- rbind(foodWebDF_up, carp_ter)
+#foodWebList <- foodWebList[-c(281)]
+
 #split into lists based on foodweb name
-foodWebList <- split( foodWebDF , f = dat$foodweb.name )
+datrescon <- foodWebDF %>% 
+  dplyr::select(res.taxonomy, con.taxonomy, foodweb.name)
+foodWebList <- split( datrescon , datrescon$foodweb.name )
+#foodWebList <- foodWebList[-c(281)]
 
 #combine iGraph data
 iglist <- mapply(graph_from_data_frame, d=foodWebList, directed=TRUE)
 igmulti <- multiweb::netData
 igcomplete <- c(iglist, igmulti)
 
+#combine df data
+colnames(foodWebDF)[4] <- "Longitude"
+colnames(foodWebDF)[5] <- "Latitude"
+colnames(foodWebDF)[3] <- "Network"
+colnames(foodWebDF)[6] <- "Ecosystem"
 
-plot_troph_level(igcomplete[[1]])
+foodWebDF["Ecosystem"][foodWebDF["Ecosystem"] == "lakes" | foodWebDF["Ecosystem"] == "streams"] <- "Dulceacuícola"
+foodWebDF["Ecosystem"][foodWebDF["Ecosystem"] == "terrestrial aboveground" | foodWebDF["Ecosystem"] == "terrestrial belowground"] <- "Terrestre"
+foodWebDF["Ecosystem"][foodWebDF["Ecosystem"] == "marine"] <- "Marino"
+
+foodWebLocation1 <- foodWebDF %>% 
+  dplyr::select(Longitude, Latitude, Network, Ecosystem)
+foodWebLocation2 <- multiweb::metadata %>%
+  dplyr::select(c(Longitude, Latitude, Network))
+foodWebLocation2$Longitude = (measurements::conv_unit(foodWebLocation2$Longitude, from = 'deg_min_sec', to = 'dec_deg') )
+foodWebLocation2$Latitude = (measurements::conv_unit(foodWebLocation2$Latitude, from = 'deg_min_sec', to = 'dec_deg') )
+foodWebLocation2$Ecosystem <- "Marino"
+
 datalist <- calc_topological_indices(igcomplete)
+uniqueLocations <- (rbind(distinct(foodWebLocation1), distinct(foodWebLocation2))) %>%
+  mutate_at(vars(Longitude,Latitude), as.numeric)
+uniqueLocations$Size <- datalist$Size
+uniqueLocations$Interactions <- datalist$Links
+uniqueLocations$popup_text <- paste0("<center>",  "</br><b>", uniqueLocations$Network,  "</br><b></br><b>Tipo de ecosystema</b>: ", uniqueLocations$Ecosystem, "</br><b>Numero de especies</b>: ", uniqueLocations$Size,  "</br><b>Numero de interacciones</b>: ", uniqueLocations$Interactions,"</center>")
 
-tic()
-calc_QSS(igcomplete[1:2], nsim = 1000)
-toc()
+plot_troph_level(igcomplete[[290]])
 
+
+#tic()
+#calc_QSS(igcomplete[1:2], nsim = 1000)
+#toc()
+
+#load(file = "~/ISP/ISP2022/R/qssResults.rda")
+#qssResults
 
